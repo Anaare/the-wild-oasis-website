5b4b0eb926700a8e468173c8469a5149c6f37b35
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-06-03 11:36:51 +0400

    Implementation of error handling

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 export const metadata = {
   title: {
     template: "%s | The Wild Oasis",
-    default: "Welcome / The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
   },
   description:
     "Luxurious cabin hotel, located in the heart of the Italian dolomites, surrounded by beautiful mountains and dark forests.",
