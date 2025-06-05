fc590c33877ddb68ed6f3fe79c723e3d0fc679f7
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-06-05 15:10:00 +0400

    Add guest creation and ID retrieval on Google sign-in via NextAuth

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
@@ -11,6 +12,22 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user; // Converts to boolean
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
